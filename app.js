a8bcf01b41fafbc0afc06592eb94de8da11d43b7
Karl Stolley <karl.stolley@gmail.com>
2020-09-14 14:40:40 -0500

    Only flash a change message on a content change

--- app.js
+++ app.js
@@ -3,10 +3,12 @@ var old_file = fs.readFileSync('./var/file.txt', {encoding:"utf8"});
 
 fs.watch('./var/file.txt', function(eventType, filename) {
   fs.readFile(`./var/${filename}`, {encoding:"utf8"}, function(err, data) {
-    console.log(`The file has this content:\n\n ${data}`);
-    console.log(data);
+    // only flash this message if the file's content has changed
+    var new_file = data;
+    if (new_file !== old_file) {
+      console.log(`The content of ${filename} has changed: it was a ${eventType} event.`)
+    }
+    old_file = new_file
   });
-
-    console.log(`${filename} has changed: it was a ${eventType} event.`)
   }
 );
