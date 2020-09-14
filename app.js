bdb0e81a5844fedd2e2f2c41b1528383a5cd56b9
Karl Stolley <karl.stolley@gmail.com>
2020-09-14 15:11:31 -0500

    Check and report diffs/additions and removals

--- app.js
+++ app.js
@@ -9,6 +9,17 @@ fs.watch('./var/file.txt', function(eventType, filename) {
     var new_file = data;
     if (new_file !== old_file) {
       console.log(`The content of ${filename} has changed: it was a ${eventType} event.`)
+      var file_changes = diff.diffLines(old_file,new_file);
+      console.log(`Here are the changes:`);
+      file_changes.forEach((change, i) => {
+        if (change.added) {
+          console.log(`Added: ${change.value}`);
+        }
+        if (change.removed) {
+          console.log(`Removed: ${change.value}`);
+        }
+      });
+
     }
     old_file = new_file
   });
