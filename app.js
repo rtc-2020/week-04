6d0ac20023c1185c31eb6c5e7b35d42ad4fabdf0
Karl Stolley <karl.stolley@gmail.com>
2020-09-16 14:16:19 -0500

    Listen for custom 'changed file' events

--- app.js
+++ app.js
@@ -1,7 +1,13 @@
 const fs = require('fs');
 const diff = require('diff');
+const {EventEmitter} = require('events');
 
 var old_file = fs.readFileSync('./var/file.txt', {encoding:"utf8"});
+var fileEvent = new EventEmitter();
+
+fileEvent.on('changed file', function(){
+  console.log('The file was changed and fired an event');
+});
 
 fs.watch('./var/file.txt', function(eventType, filename) {
   fs.promises.readFile(`./var/${filename}`, {encoding:"utf8"})
@@ -11,6 +17,7 @@ fs.watch('./var/file.txt', function(eventType, filename) {
     if (new_file !== old_file) {
       console.log(`The content of ${filename} has changed: it was a ${eventType} event.`)
       var file_changes = diff.diffLines(old_file,new_file);
+      /*
       console.log(`Here are the changes (promise!):`);
       file_changes.forEach((change, i) => {
         if (change.added) {
@@ -20,7 +27,8 @@ fs.watch('./var/file.txt', function(eventType, filename) {
           console.log(`Removed: ${change.value}`);
         }
       });
-
+      */
+      fileEvent.emit('changed file');
     }
     old_file = new_file
   });
