26650657a198716f9f4f97b099a2b214d68f2805
Karl Stolley <karl.stolley@gmail.com>
2020-09-18 15:39:02 -0500

    Handled 'diffed changes' devent in the browser

--- socket-express/public/javascripts/site.js
+++ socket-express/public/javascripts/site.js
@@ -4,3 +4,7 @@ socket.on('message', function(data) {
   console.log('Message received: ' + data);
   socket.emit('message received', 'boom! I got your message');
 });
+
+socket.on('diffed changes', function(data) {
+  console.log(`File changed: ${data}`);
+});
